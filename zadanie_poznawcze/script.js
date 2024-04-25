f24ecbcd29551c48dd00a9704d2913764e077491
Bartosz Kuniński <bartosz.kuninski@gmail.com>
2024-04-25 14:44:18 +0200

    Add all project files

--- zadanie_poznawcze/script.js
+++ zadanie_poznawcze/script.js
@@ -81,7 +81,7 @@ window.onload = function() {
         if (responseForm) {
             responseForm.style.display = 'none';
             if (currentVideoIndex === 0) {
-                alert("Poprawną odpowiedzią było 'Figura jest trójkątem'. Jeśli zaznaczyłeś/aś poprawnie - gratulacje! Teraz czekają Cię 4 właściwe zadania. Każde zadanie zawiera po 12 figur i trwa po 1 minucie. Powodzenia!.");
+                alert("Poprawną odpowiedzią było 'Figura jest trójkątem'. Jeśli zaznaczyłeś/aś poprawnie - gratulacje! Teraz czekają Cię 4 właściwe zadania. Każde zadanie zawiera po 12 figur i trwa po 1 minucie. Po odpowiedzi NIE będzie już feedbacku, która odpowiedź była poprawna. Powodzenia!.");
             }
 
             if (currentVideoIndex < videos.length - 1) {
